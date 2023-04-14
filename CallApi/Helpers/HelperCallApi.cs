4a2daf7a6e0993cdab51a2a3358c1b178d10e28b
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-04-14 12:49:09 +0200

    Add project files.

---
+++ CallApi/Helpers/HelperCallApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallApi.Helpers
+{
+    public static class HelperCallApi
+    {
+        public async static Task<T> CallApiAsync<T>(string uri, string request, MediaTypeWithQualityHeaderValue headers)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(uri);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(headers);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+    }
+}
