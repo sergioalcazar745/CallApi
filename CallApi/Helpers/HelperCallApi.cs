8ca5c8413acc6c7ce87af15c74768c4c2f605dab
Sergio Alcázar Monedero <60264314+sergioalcazar745@users.noreply.github.com>
2023-05-02 20:59:36 +0200

    delete using and models

--- CallApi/Helpers/HelperCallApi.cs
+++ CallApi/Helpers/HelperCallApi.cs
@@ -1,13 +1,7 @@
-﻿using CallApi.Models;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CallApi.Helpers
 {
